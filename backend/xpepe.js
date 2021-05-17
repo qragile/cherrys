87ced2a32ad2b54efe733822d84ec2f87b20b4f7
Qragile Atlas <edgar.gonzalez@qragile.uy>
2021-05-17 13:43:49 -0300

    ECOVOY 4.0.1 HEROku
    va

--- backend/xpepe.js
+++
@@ -1,7 +0,0 @@
-mongodump --uri "mongodb+srv://<your username>:<your password>@<your cluster>.mongodb.net/sample_supplies"
-
-mongoexport --uri="mongodb+srv://<your username>:<your password>@<your cluster>.mongodb.net/sample_supplies" --collection=sales --out=sales.json
-
-mongorestore --uri "mongodb+srv://<your username>:<your password>@<your cluster>.mongodb.net/sample_supplies"  --drop dump
-
-mongoimport --uri="mongodb+srv://<your username>:<your password>@<your cluster>.mongodb.net/sample_supplies" --drop sales.json
