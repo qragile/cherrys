93cf95762460cb43c58930f2f5039e7d8e4d0242
Qragile Atlas <edgar.gonzalez@qragile.uy>
2021-05-25 16:17:32 -0300

    meli version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,6 +49,28 @@ function App() {
     error: errorCategories,
     categories,
   } = productCategoryList;
+
+  app.get('/posts', validateToken, async (req, res) => {
+    try {
+      const meliObject = new MeliObject(res.locals.access_token);    
+      const user = await meliObject.get('/users/me');
+      const items = (await meliObject.get(`/users/${user.id}/items/search`)).results || [];
+      if (items.length) {
+        const result = [];
+        const promises = items.map(item_id => meliObject.get(`/items/${item_id}`));
+        for await (item of promises) {
+          result.push(item);
+        }
+        res.render('posts', { items: result });
+      } else {
+        res.status(404).send('no items were found :(');
+      }
+    } catch(err) {
+      console.log('Something went wrong', err);
+      res.status(500).send(`Error! ${err}`);
+    }
+  });
+  
   useEffect(() => {
     dispatch(listProductCategories());
   }, [dispatch]);
@@ -283,24 +305,5 @@ function App() {
     </BrowserRouter>
   );
 }
-app.get('/posts', validateToken, async (req, res) => {
-  try {
-    const meliObject = new MeliObject(res.locals.access_token);    
-    const user = await meliObject.get('/users/me');
-    const items = (await meliObject.get(`/users/${user.id}/items/search`)).results || [];
-    if (items.length) {
-      const result = [];
-      const promises = items.map(item_id => meliObject.get(`/items/${item_id}`));
-      for await (item of promises) {
-        result.push(item);
-      }
-      res.render('posts', { items: result });
-    } else {
-      res.status(404).send('no items were found :(');
-    }
-  } catch(err) {
-    console.log('Something went wrong', err);
-    res.status(500).send(`Error! ${err}`);
-  }
-});
+
 export default App;
